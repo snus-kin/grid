69c1a6cfb0e8df721acaa752730ccd07e4cce194
snus-kin <tcarroll@snufk.in>
2021-01-31 10:57:32 +0000

    Add samples, start work on synth, using teoria temporarily at least

--- js/script.js
+++ js/script.js
@@ -1,85 +1,207 @@
-new p5((p5) => {
-    var x = 0;
-    var y = 0;
-    var z = 0;
-    var isUp, isDown, isLeft, isRight, isIn, isOut;
+// TODO GRANULAR
+/*
+const churchPlayer = new Tone.GrainPlayer({
+    url: "samples/church.wav",
+    loop: true,
+    grainSize: 0.1,
+    overlap: 0.5,
+}).toDestination();
 
-    rows = 20;
-    cols = 20;
-    gridSize = 30;
+const bellPlayer = new Tone.GrainPlayer({
+    url: "samples/bell.wav",
+    loop: true,
+    grainSize: 0.1,
+    overlap: 1,
+}).toDestination();
 
-    p5.setup = () => {
-        p5.createCanvas(p5.windowWidth, p5.windowHeight);
-        p5.background(255);
-    }
+churchPlayer.sync().start(0).stop(50);
+bellPlayer.sync().start(0).stop(50);
 
-    p5.draw = () => {
-        p5.background(255);
-
-        // grid
-        // ---
-        p5.strokeWeight(2);
-        p5.stroke(0);
-
-        p5.translate((p5.windowWidth/2) + (0-x), (p5.windowHeight/2) + (0-y));
-
-        var sx, sy, cx, cy;
-        for (j = 0; j < rows; j++) {
-            for (i = 0; i < cols; i++) {
-                sx = (x + (gridSize * ((cols/2) - i)));
-                sy = (y + (gridSize * ((cols/2) - j)));
-                cx = sx + gridSize;
-                cy = sy + gridSize;
-                
-                p5.beginShape();
-                    p5.vertex(sx, sy + limit(z, sy+gridSize/4));
-                    p5.vertex(sx + limit(z, sx+gridSize/4), cy);
-                    p5.vertex(cx, cy - limit(z, cy-gridSize/4));
-                    p5.vertex(cx + limit(z, cx-gridSize/4), sy);
-                p5.endShape(p5.CLOSE);
-            }
-        }
-        // --- 
-
-        if (isUp) y--;
-        if (isDown) y++;
-        if (isLeft) x--;
-        if (isRight) x++;
-        if (isIn) z--;
-        if (isOut) z++;
-    }
-
-    p5.windowResized = () => {
-        resizeCanvas(windowWidth, windowHeight);
-    }
-
-    p5.keyPressed = () => {
-        setMove(p5.key, true);
-    }
-
-    p5.keyReleased = () => {
-        setMove(p5.key, false);
-    }
-
-    function setMove(k, b) {
-        if (k == 'w') { isUp = b };
-        if (k == 's') { isDown = b };
-        if (k == 'a') { isLeft = b };
-        if (k == 'd') { isRight = b };
-        if (k == 'e') { isIn = b };
-        if (k == 'c') { isOut = b };
-    }
-
-    function limit(value, amplitude) {
-        return amplitude * p5.sin((p5.PI / amplitude) * value);
-    }
-}, document.querySelector("#content"));
-
-// TRANSPORT
-Tone.Transport.loopStart = 0;
-Tone.Transport.loopEnd = "1:0";
 Tone.Transport.loop = true;
+*/
 
-// start / stop transport
-document.querySelector("tone-play-toggle").addEventListener("start", e => Tone.Transport.start());
-document.querySelector("tone-play-toggle").addEventListener("stop", e => Tone.Transport.stop());
+/*
+ * ============
+ * Setup Synths
+ * ============
+ */
+let destinationGain = 0.3;
+let voiceIndex = 0;
+const NUM_OSCS = 4;
+
+const hpFilter = new Tone.Filter({ frequency: 5, type: "highpass" });
+const lpFilter = new Tone.Filter(20000, "lowpass");
+const cheby = new Tone.Chebyshev({ order: 2, wet: 0 });
+const limiter = new Tone.Limiter();
+const fft = new Tone.FFT();
+
+const ACTIVE_EFFECTS = [cheby, hpFilter, lpFilter];
+const DESTINATION_OUTPUT = new Tone.Gain(destinationGain).fan(
+    Tone.Destination,
+    fft
+);
+const FX_BUS = new Tone.Gain(0.2).chain(...ACTIVE_EFFECTS, DESTINATION_OUTPUT);
+
+// base synth
+const synthb = new AdditiveSynth();
+// this is the one that will be detuned as per our x,y 
+const synthm = new AdditiveSynth();
+const voices = [synthb, synthm];
+
+const initialOscs = voices[0].getOscs();
+/* 
+ * =======
+ * Utility
+ * =======
+ */
+function setMove(k, b) {
+    if (k == 'w') { isUp = b };
+    if (k == 's') { isDown = b };
+    if (k == 'a') { isLeft = b };
+    if (k == 'd') { isRight = b };
+    if (k == 'e') { isIn = b };
+    if (k == 'c') { isOut = b };
+}
+
+function limit(value, amplitude) {
+    return amplitude * sin((PI / amplitude) * value);
+}
+
+function toggleTransport() {
+  // toggles the transportthe transport
+  Tone.Transport.toggle();
+  if (Tone.Transport.state === "stopped") {
+    voices.forEach((synth) => {
+      synth.triggerRelease();
+    });
+  }
+}
+
+/* 
+ * ===============
+ * p5js grid part
+ * + synth control
+ * ===============
+ */
+var x = 0;
+var y = 0;
+var z = 0;
+var interval;
+
+var isUp, isDown, isLeft, isRight, isIn, isOut;
+
+var rows = 20;
+var cols = 20;
+var gridSize = 30;
+var gridSpacing = 10;
+
+let chordIndex = 0;
+let octaveMultiplier = 1;
+let chordNoteIndex = 0;
+const scale = teoria.note("a").scale("major");
+
+const getChord = (i) => [
+  scale.get(i).fq(),
+  scale.get(i + 2).fq(),
+  scale.get(i + 4).fq(),
+  scale.get(i + 6).fq(),
+];
+
+const playVoice = (note, time) => {
+  // const voices = [synth, synth1, synth2];
+  // const prevIndex = voiceIndex;
+  voices[voiceIndex].triggerRelease(note, time);
+  voiceIndex++;
+  voiceIndex = voiceIndex % voices.length;
+  voices[voiceIndex].triggerAttack(note, time);
+};
+
+Tone.Transport.scheduleRepeat((time) => {
+  const chord = getChord(chordIndex);
+
+  playVoice(chord[chordNoteIndex] * octaveMultiplier, time);
+
+  chordNoteIndex++;
+  chordNoteIndex = chordNoteIndex % chord.length;
+}, "4n");
+
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+    background(255);
+
+    Tone.Transport.toggle();
+}
+
+function draw() {
+    background(255);
+
+    // grid
+    // ---
+    strokeWeight(2);
+    stroke(0);
+
+    translate((innerWidth/2) + (0-x), (innerHeight/2) + (0-y));
+    
+    // draw the actual grid, here some offsets should be calculated by another
+    // function or something?
+    var sx, sy, cx, cy;
+    var ox = 0;
+    var oy = 0;
+    for (j = 0; j < rows; j++) {
+        ox = 0
+        for (i = 0; i < cols; i++) {
+            sx = (x + (gridSize * ((cols/2) - i)));
+            sy = (y + (gridSize * ((cols/2) - j)));
+            sz = (z + (gridSize * (cols/2)));
+            cx = sx + gridSize;
+            cy = sy + gridSize;
+            
+            beginShape();
+                vertex(sx - limit(sz-sx, gridSize/4), sy + limit(sz+sy, gridSize));
+                vertex(sx - limit(sz-sx, gridSize/4), cy + limit(sz+cy, gridSize));
+                vertex(cx + limit(sz+cx, gridSize/4), cy - limit(sz-cy, gridSize));
+                vertex(cx + limit(sz+cx, gridSize/4), sy - limit(sz-sy, gridSize));
+            endShape(CLOSE);
+
+            ox += gridSpacing;
+        }
+        oy += gridSpacing;
+    }
+    // --- 
+
+    // this is where we work out what the interval should be
+    //interval = 3**x * 5**y * 7**z;
+
+    //while (interval < 1 || interval >=2) {
+    //    if (interval <1) {
+    //        interval = interval * 2;
+    //    } else {
+    //        interval = interval / 2;
+    //    }
+    //}
+
+    if (isUp) y--;
+    if (isDown) y++;
+    if (isLeft) x--;
+    if (isRight) x++;
+    if (isIn) z--;
+    if (isOut) z++;
+}
+
+function windowResized() {
+    resizeCanvas(innerWidth, innerHeight);
+}
+
+function keyPressed() {
+    setMove(key, true);
+
+    if (key === " ") {
+        Tone.start().then(() => {
+            toggleTransport();
+        });
+    }
+}
+
+function keyReleased() {
+    setMove(key, false);
+}
---
+++ js/synths.js
@@ -0,0 +1,101 @@
+/* 
+ *  This file defines the synths to be used in the main p5js script
+ */
+
+function AdditiveSynth() {
+    // adapted from : https://github.com/ejarzo/additive-synth/blob/master/synth.js
+    this.output = new Tone.Gain().connect(FX_BUS);
+    this.synths = [... new Array(NUM_OSCS)].map((_, i) => {
+        const omniOsc = new Tone.OmniOscillator({
+            type: "sine",
+            phase: (i / NUM_OSCS) * 360,
+            volume: 0 - i *2,
+        });
+        
+        // ADSR
+        const env = new Tone.AmplitudeEnvelope({
+            attack:0.2,
+            decay: 2,
+            sustain: 1,
+            release: 0.3
+        });
+
+        omniOsc.chain(env);
+
+        const loop = new Tone.Loop((time) => {
+            env.triggerAttack(time);
+        });
+
+        const isLooping = true;
+
+        return {harmonic: i + 1, omniOsc, env, loop, isLooping };
+    });
+
+    this.synths[0].env.connect(this.output);
+    
+    for (let i = 1; i < this.synths.length; i++) {
+        this.synths[i].env.connect(this.synths[0].env);
+    }
+
+    const getOscs = () => this.synths;
+
+    return {
+        getOscs,
+
+        setOscType: (oscIndex, type) => {
+            this.synths[oscIndex].omniOsc.set({ type });
+        },
+
+        setLoop: (osc, interval) => {
+            console.log(interval);
+            if (interval === "off") {
+                this.synths[oscIndex].isLooping = false;
+                this.synths[oscIndex].loop.cancel();
+            } else {
+                this.synths[oscIndex].loop.set({ interval });
+                this.synths[oscIndex].isLooping = true;
+            }
+        },
+
+        setHarmonic: (oscIndex, harmonic) => {
+            this.synths[oscIndex].harmonic = harmonic;
+        },
+
+        setVolume: (oscIndex, volume) => {
+            this.synths[oscIndex].omniOsc.volume.value =
+                volume === -24 ? -Infinity : volume;
+        },
+
+        setDetune: (oscIndex, detune) => {
+            this.synths[oscIndex].omniOsc.set({ detune });
+        },
+
+        setEnvValue: (oscIndex, param, val) => {
+            this.synths[oscIndex].env[param] = val;
+        },
+
+        triggerAttack: (note, time) => {
+            getOscs().forEach(({ omniOsc, env, harmonic, loop, isLooping }, i) => {
+                const fq = note * (harmonic === 0 ? 0.5 : harmonic);
+                omniOsc.frequency.value = fq;
+                if (omniOsc.state === "stopped") {
+                    omniOsc.start(time);
+                }
+
+                if (isLooping) {
+                    loop.cancel();
+                    loop.start();
+                } else {
+                    loop.cancel();
+                    env.triggerAttack(time);
+                }
+            });
+        },
+
+        triggerRelease: (time) => {
+            getOscs().forEach(({ omniOsc, env }, i) => {
+                env.triggerRelease();
+            });
+        },
+    };
+}
